16f5005232084460564fa772edbf23b0dd025427
Skylar Bowen <bowenskylar>
2016-10-26 14:15:03 -0400

    HELLO WORLD

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const express = require('express');
 const logger = require('morgan');
+const { findArtist } = require('./services/beer');
+
 
 const app = express();
-
 const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
-
 app.listen(port, () => console.log('Server is running on port', port));
 
 app.set('view engine', 'ejs');
@@ -17,6 +19,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.render('index', {
-    heading: 'Hello Skylar',
+    heading: 'Hello Skylar!!',
   });
 });
--- services/beer.js
+++ services/beer.js
@@ -1,7 +1,22 @@
 const fetch = require('node-fetch');
-const API_URL = ''// API_URL
+const API_URL = 'http://api.brewerydb.com/v2/'; //  API_URL version two
 
-//function to get beer name
+//  function to get beer name
+
+function getBeerByName(req, res, next) {
+  console.log(req.body);
+  fetch(`${API_URL}key=${req.body.searchKey}`)
+  .then(r => r.json())
+  .then((data) => {
+    res.results = data.results;
+    next();
+  })
+.catch((err) => {
+  res.err = err;
+  next();
+});
+}
 
 
-//module.exports ={function name here};
+//  module.exports ={function name here};
+module.exports = {getBeerByName};
